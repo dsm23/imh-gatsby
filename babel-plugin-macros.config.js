3862675a101db80ce16557ebf670cb12e0e994d6
David Murdoch <dsmurdoch357@gmail.com>
2022-08-25 18:40:14 +0100

    babel changes, use gatsby preset

--- babel-plugin-macros.config.js
+++ babel-plugin-macros.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   twin: {
-    config: 'tailwind.config.js',
     preset: 'styled-components',
-    autoCssProp: true,
-    debugProp: true,
-    debugPlugins: false,
-    debug: false,
   },
 }
---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { "legacy": true }]
+  ],
+  presets: [
+    [
+      "babel-preset-gatsby",
+      {
+        targets: {
+          browsers: [">0.25%", "not dead"]
+        }
+      }
+    ]
+  ]
+};
