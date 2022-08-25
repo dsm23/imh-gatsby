0f49eb7ee1fef1525a7cf7c7936790abf1cf2a42
David Murdoch <dsmurdoch357@gmail.com>
2022-08-25 18:39:26 +0100

    Use inter font from fontsource

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,7 @@
-import 'typeface-inter';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
 import './src/styles.css'
 
 export const onServiceWorkerUpdateReady = () => {
