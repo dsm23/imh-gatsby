8059d34fbdc357714760f6532222a19f2766b37d
David Murdoch <dsmurdoch357@gmail.com>
2022-08-25 18:40:50 +0100

    postcss use object syntax

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const tailwind = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-
 module.exports = {
-  plugins: [tailwind],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
